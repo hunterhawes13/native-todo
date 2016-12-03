b9237906a70e1fd9b1da6a47b3872024b3749ca0
Hunter Hawes <hunterhawes13@gmail.com>
2016-12-03 15:38:56 -0600

    Sat Dec  3 15:38:56 CST 2016

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+const nums = [1,2,3,4]
 export class Todo extends Component {
+
   render() {
     return (
-      <div>hello</div>
+    	<div>
+      		<input type="text" />
+      		<button>click</button>
+      		{nums.map(num => (<h3>{num * 2 + " yo"}</h3>))}
+     	</div>
     );
   }
 }
